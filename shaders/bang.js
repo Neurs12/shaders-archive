d9678121f4e7cfdf8e6cc046f0c30cec42f106d4
Neurs <31346263+Neurs12@users.noreply.github.com>
2024-03-30 18:56:20 +0700

    Create bang.js

---
+++ shaders/bang.js
@@ -0,0 +1,24 @@
+const vertexShader = `
+uniform float uTime;
+uniform float uRadius;
+precision mediump float;
+
+void main() {
+    float distanceFactor = pow(uRadius - distance(position, vec3(0.0)), 1.5);
+    vec3 particlePosition = position * (uTime * 0.3 * distanceFactor);
+
+    vec4 modelPosition = modelMatrix * vec4(particlePosition, 1.0);
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectedPosition;
+    gl_PointSize = 3.0;
+}
+`;
+
+const fragmentShader = `
+void main() {
+    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
+`;
